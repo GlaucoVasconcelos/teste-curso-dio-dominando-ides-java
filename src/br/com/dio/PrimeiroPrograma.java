31b4dce391c734259524ed3a40e8b5a120e07f3a
Glauco <vasconcelo.50@hotmail.com>
2021-10-25 12:13:12 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+  public static void main(String[] args) {
+	  
+	  Gato gato = new Gato();
+	  Livros Livros = new Livros();
+	  
+	  System.out.println(gato);
+	  System.out.println(Livros);
+	  /*int a = 5;
+	  int b = 12;
+	System.out.println("Olá Samantha " + (a+b));*/
+    }
+  
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
